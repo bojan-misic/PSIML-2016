e0d73e5401c0c1e86df1dc8afb49428fbaacac14
Bojan Misic <misic.bojan@gmail.com>
2016-05-22 19:10:21 +0200

    Implement C# solution to Historgram problem

---
+++ Solutions/Histogram/Src/Histogram/Histogram/Histogram.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Histogram
+{
+    public class Histogram
+    {
+        private Dictionary<byte, uint> histogram = new Dictionary<byte, uint>();
+        private uint numberOfPixels = 0;
+        private Random rand = new Random();
+
+        public Histogram()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            histogram.Clear();
+
+            // Initialize histogram
+            for (byte i = 0;; i++)
+            {
+                histogram[i] = 0;
+                if (i == 255) break;
+            }
+        }
+
+        public void ProcessBitmap(FileInfo bitmapFile, FileInfo textFile)
+        {      
+            uint[] cropArea = new uint[4];
+            // Get crop area from text file
+            using (StreamReader sr = textFile.OpenText())
+            {
+                var cropAreaString = sr.ReadToEnd().Split(' ');
+                for (uint i = 0; i < cropAreaString.Length; i++)
+                {
+                    try
+                    {
+                        cropArea[i] = uint.Parse(cropAreaString[i]);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+
+            uint left = cropArea[0];
+            uint top = cropArea[1];
+            uint width = cropArea[2];
+            uint height = cropArea[3];
+
+            var bitmap = new Bitmap(bitmapFile.FullName);
+            for (uint x = left; x < left + width; x++)
+            {
+                for (uint y = top; y < top + height; y++)
+                {
+                    Color pixel = bitmap.GetPixel((int)x, (int)y);
+                    byte grayscale = Convert.ToByte(pixel.R * 0.2126 + pixel.G * 0.7152 + pixel.B * 0.0722);
+
+                    histogram[grayscale] = histogram[grayscale] + 1;
+                    numberOfPixels++;
+                }
+            }
+        }
+
+        public Bitmap GenerateRandomGrayscaleBitmapFromHistogram(uint size)
+        {
+            var scaledHistogram = getScaledHistogram(size);
+
+            var bitmap = new Bitmap((int)size, (int)size, PixelFormat.Format24bppRgb);
+            var pixels = new byte[size * size];
+
+            for (int i = 0; i < size*size; i++)
+            {
+                byte? nullableIntensity = getRandomIntensity(scaledHistogram);
+                if (nullableIntensity.HasValue)
+                {
+                    byte intensity = nullableIntensity.Value;
+                    scaledHistogram[intensity]--;
+
+                    pixels[i] = intensity;
+                }
+                else
+                {
+                    pixels[i] = 0;
+                }
+            }
+
+            new Random().Shuffle(pixels);
+
+            for (uint x = 0; x < size; x++)
+            {
+                for (uint y = 0; y < size; y++)
+                {
+                    var intensity = pixels[x*size + y];
+
+                    bitmap.SetPixel((int)x, (int)y, Color.FromArgb(intensity, intensity, intensity));
+                }
+            }
+
+            return bitmap;
+        }
+
+        private byte? getRandomIntensity(Dictionary<byte, uint> histogram)
+        {
+            var filteredHistogram = histogram.Where(item => item.Value > 0);
+            if (filteredHistogram.Any())
+            {
+                return filteredHistogram.ElementAt(rand.Next(0, filteredHistogram.Count())).Key;
+            }
+
+            return null;
+        }
+
+        private Dictionary<byte, uint> getScaledHistogram(uint size)
+        {
+            uint ratio = size * size / numberOfPixels;
+            var scaledHistogram = new Dictionary<byte, uint>(histogram);
+
+            for (byte i = 0; ; i++)
+            {
+                scaledHistogram[i] = scaledHistogram[i] * ratio;
+                if (i == 255) break;
+            }
+
+            return scaledHistogram;
+        }
+
+        private string toString(Dictionary<byte, uint> histogram)
+        {
+            var sb = new StringBuilder();
+
+            for (byte i = 0;; i++)
+            {
+                sb.AppendFormat("{0},", histogram[i]);
+                if (i == 255) break;
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return toString(histogram);
+        }
+
+        public string ToCsv()
+        {
+            return ToString();
+        }
+
+        public string toCsvScaled(uint size)
+        {
+            return toString(getScaledHistogram(size));
+        }
+    }
+}
---
+++ Solutions/Histogram/Src/Histogram/Histogram/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid arguments provided. Usage: histogram.exe inputFolderPath outputFolderPath");
+                return;
+            }
+
+            var inputFolderPath = args[0];
+            var outputFolderPath = args[1];
+            var histogram = new Histogram();
+
+            var fileList = new List<System.IO.FileInfo>();
+            Helpers.TraverseTree(inputFolderPath, fileList);
+
+            foreach (var file in fileList)
+            {
+                if (file.Extension != ".bmp")
+                {
+                    continue;
+                }
+
+                // We have a bitmap file at this point
+                var textFileName = file.FullName.Replace("bmp", "txt");
+                try
+                {
+                    var textFile = new System.IO.FileInfo(textFileName);
+                    histogram.ProcessBitmap(file, textFile);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            string globalCsv = histogram.ToCsv();
+            string matchingCsv = histogram.toCsvScaled(100);
+
+            var globalHistoFile = new System.IO.FileInfo(outputFolderPath + "\\GlobalHisto.csv");
+            globalHistoFile.Directory.Create(); // If the directory already exists, this method does nothing.
+            System.IO.File.WriteAllText(globalHistoFile.FullName, globalCsv);
+
+            var matchingHistoFile = new System.IO.FileInfo(outputFolderPath + "\\MatchingHisto.csv");
+            System.IO.File.WriteAllText(matchingHistoFile.FullName, matchingCsv);
+
+            var matchedBitmap = histogram.GenerateRandomGrayscaleBitmapFromHistogram(100);
+            matchedBitmap.Save(outputFolderPath + "\\MatchingImage.bmp");
+        }
+    }
+}
